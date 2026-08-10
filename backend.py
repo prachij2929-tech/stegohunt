686997e9ceeda18db978bd708e2538078d3d051b
prachij2929-tech <prachij2929@gmail.com>
2026-08-10 19:07:11 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -39,11 +39,11 @@ app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
 # ---------------- DATABASE CONNECTION ----------------
 def get_db_connection():
     return mysql.connector.connect(
-        host=os.getenv("DB_HOST", "localhost"),
+        host=os.getenv("DB_HOST", "tokaido.proxy.rlwy.net"),
         user=os.getenv("DB_USER", "root"),
         password=os.getenv("DB_PASSWORD", ""),
-        database=os.getenv("DB_NAME", "stegnohunt"),
-        port=int(os.getenv("DB_PORT", "3307"))
+        database=os.getenv("DB_NAME", "railway"),
+        port=int(os.getenv("DB_PORT", "49426"))
     )
 
 
